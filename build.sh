f025d4c9bc142cdba628a571b349c4a8c77776e5
Yuriy Solovyov <yuriy.solovyov@prism.com>
2019-09-30 17:37:38 +0300

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+conan create . bvnp43/stable \
+    -o opencv:ffmpeg=True \
+    -o opencv:contrib=True \
+    -o opencv:harfbuzz=False \
+    -o opencv:freetype=False \
+    -o opencv:webp=False \
+    -o opencv:tiff=False \
+    --build missing
