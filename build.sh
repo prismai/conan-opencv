471eebef3dd09f8f84c54c6c696f89c1b85bdda2
Yuriy Solovyov <yuriy.solovyov@prism.com>
2019-11-15 16:24:32 +0200

    fixes

--- build.sh
+++ build.sh
@@ -4,10 +4,18 @@ set -e
 
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan -f
 conan create . bvnp43/stable \
-    -o opencv:ffmpeg=True \
     -o opencv:contrib=True \
-    -o opencv:harfbuzz=False \
+    -o opencv:dc1394=False \
+    -o opencv:eigen=False \
+    -o opencv:ffmpeg=True \
     -o opencv:freetype=False \
-    -o opencv:webp=False \
+    -o opencv:gflags=False \
+    -o opencv:glog=False \
+    -o opencv:harfbuzz=False \
+    -o opencv:jasper=False \
+    -o opencv:openexr=False \
+    -o opencv:png=True \
+    -o opencv:quirc=False \
     -o opencv:tiff=False \
-    --build missing
+    -o opencv:webp=False \
+    --build outdated
